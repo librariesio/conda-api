0fa85e5609879aa94d26946f03a2fe6cbcbd2303
Tyrel Souza <923113+tyrelsouza@users.noreply.github.com>
2019-09-09 16:50:38 -0400

    wth, i lost code

--- app.rb
+++ app.rb
@@ -13,15 +13,26 @@ class CondaAPI < Sinatra::Base
     "Hello World! #{CONDA_REPO.package_names.length} \n"
   end
 
-  get "/packages.json" do
+  get "/packages" do
     content_type :json
     CONDA_REPO.package_names.to_json
   end
 
-  get "/packages/:name.json" do
+  get "/packages/:name" do
     content_type :json
     channel = "pkgs/main" # Default for now
-    package = CONDA_REPO.package(channel, params[:name])
+    package_version(channel, name)
+  end
+
+  get %r{/packages/(?<channel>.*)/(?<name>.*)/?} do
+    content_type :json
+    package_version(params[:channel], params[:name])
+  end
+
+  private
+
+  def package_version(channel, name)
+    package = CONDA_REPO.package(channel, name)
     if package
       package.to_json
     else
