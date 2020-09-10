cef68d702825c27e692f0cd2756460c0d5c79116
Matt Ellis <mellis@tidelift.com>
2020-09-10 15:21:32 -0400

    Add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+REVISION=$(git show-ref origin/master |cut -f 1 -d ' ')
+TAGGED_IMAGE=gcr.io/${GOOGLE_PROJECT}/conda-api:${REVISION}
+gcloud --quiet container images describe ${TAGGED_IMAGE} || { status=$?; echo "Container not finished building" >&2; exit $status; }
+
+gcloud --quiet container images add-tag ${TAGGED_IMAGE} gcr.io/${GOOGLE_PROJECT}/conda-api:latest
+
+kubectl set image deployment/conda-service conda-service=${TAGGED_IMAGE}
